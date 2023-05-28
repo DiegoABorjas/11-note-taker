5dba0ca5fe99afcd4d66e39b1082f7010240d318
Diego Borjas <diegoborjas@gmail.com>
2023-05-28 18:03:34 -0500

    added basic routes to see all notes

---
+++ routes/notes.js
@@ -0,0 +1,18 @@
+const notes = require('express').Router()
+
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+
+
+
+module.exports = notes
