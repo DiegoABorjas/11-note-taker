84b65f897a1ec431deefb2632ef31920b734b41b
Diego Borjas <diegoborjas@gmail.com>
2023-05-30 20:03:14 -0500

    updated routes so getting a specific note works

--- routes/notes.js
+++ routes/notes.js
@@ -12,12 +12,12 @@ notes.get('/', (req, res) => {
 });
 
 // GET Route for a specific note
-notes.get('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
+notes.get('/:id', (req, res) => {
+    const noteId = req.params.id;
     readFromFile('./db/notes.json')
       .then((data) => JSON.parse(data))
       .then((json) => {
-        const result = json.filter((note) => note.note_id === noteId)
+        const result = json.filter((note) => note.id === noteId)
         return result.length > 0
           ? res.json(result)
           : res.json('No note with that ID');
@@ -34,7 +34,7 @@ notes.post('/', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuidv4(),
+        id: uuidv4(),
       };
   
       readAndAppend(newNote, './db/db.json')
